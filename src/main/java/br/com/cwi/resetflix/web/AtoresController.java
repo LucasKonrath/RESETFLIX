a538c030e7458dc93d67de7a0ad2d31abb7e8dd6
Lucas Konrath Damaceno <lucas.damaceno@cwi.com.br>
2021-02-27 17:59:53 -0300

    adicionar resolucao resetflix

--- src/main/java/br/com/cwi/resetflix/web/AtoresController.java
+++ src/main/java/br/com/cwi/resetflix/web/AtoresController.java
@@ -3,6 +3,7 @@ package br.com.cwi.resetflix.web;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,31 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.cwi.resetflix.request.CriarAtorRequest;
 import br.com.cwi.resetflix.response.AtoresResponse;
 import br.com.cwi.resetflix.response.ConsultarDetalhesAtorResponse;
+import br.com.cwi.resetflix.service.AtoresService;
 
 @RestController
 @RequestMapping("/atores")
 public class AtoresController implements AtoresContract {
 
-    //TODO IMPLEMENTAR SERVICE DE ATORES
+    @Autowired
+    private AtoresService atoresService;
 
     @Override
     @GetMapping
     public List<AtoresResponse> getAtores() {
-        //TODO IMPLEMENTAR CHAMADA DE SERVICE
-        return Collections.emptyList();
+        return atoresService.getAtores();
     }
 
     @Override
     @GetMapping("/{id}")
     public ConsultarDetalhesAtorResponse getAtorById(@PathVariable("id") final Long id) {
-        //TODO IMPLEMENTAR CHAMADA DE SERVICE
-        return null;
+        return atoresService.consultarDetalhesAtor(id);
     }
 
     @Override
     @PostMapping
     public Long criarAtor(@RequestBody final CriarAtorRequest request) {
-        //TODO IMPLEMENTAR CHAMADA DE SERVICE
-        return null;
+        return atoresService.criarAtor(request);
     }
 }
